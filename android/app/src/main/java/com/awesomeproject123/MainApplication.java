2feaf45fafa1ef5c7606a23def1b3c6bce8ebe1b
pushkar.goel <pushkar.goel@paytm.com>
2018-11-29 12:52:05 +0530

    android stable build

--- android/app/src/main/java/com/awesomeproject123/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject123/MainApplication.java
@@ -3,7 +3,7 @@ package com.awesomeproject123;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -28,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImagePickerPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage(),
             new RNSoundPackage(),
